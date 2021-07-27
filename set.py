de4025f7bbe52df828a034a777709491413faa16
Tharindu97 <tharindukavishna@gmail.com>
2021-07-27 08:50:35 +0530

    learning set

---
+++ set.py
@@ -0,0 +1,25 @@
+# x = {1,7,3,4,4}
+# x.add(9)
+# print(x)
+
+# x.update([2,6,8])
+# print(x)
+
+# x.remove(1)
+# print(x)
+
+# del x
+# print(x)
+#
+# x = [1,2,3,4]
+# y = set(x)
+# print(type(x))
+# print(type(y))
+
+x = {1,2,3,4,5,6}
+y = {2,4,6,8}
+# print(x & y)
+# print(x.intersection(y))
+
+# print(x | y)
+# print(x.union(y))
