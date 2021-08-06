cb083186ed06edc727d6d1895d45404cbcc10e46
Tharindu97 <tharindukavishna@gmail.com>
2021-08-06 16:30:03 +0530

    learn slicing and negative indexing

---
+++ breakandcontinuestatement.py
@@ -0,0 +1,14 @@
+for x in range(1 , 6):
+    if(x==3):
+        break
+    print(x)
+
+
+print("**************")
+
+i = 0
+while(i<5):
+    i += 1
+    if (i == 3):
+        break
+    print(i)
---
+++ loops.py
@@ -0,0 +1,32 @@
+# while loop
+# x = 2
+# while(x<=10):
+#     print(x)
+#     x+=2
+
+# x = 1
+# sum = 0
+# while(x<=5):
+#    number = int(input("Enter the Number: "))
+#    sum = sum + number
+#    x+=1
+# print(sum)
+
+# For Loop
+# x = [1,2,3,4,5]
+#
+# for num in x:
+#     print(num)
+
+# x = {
+#     "name": "Tharindu",
+#     "age": 23,
+#     "gender": "male"
+# }
+
+# for key,value in x.items():
+#     print(key,value)
+
+# for i in range(2,11,2):
+#     print(i)
+
