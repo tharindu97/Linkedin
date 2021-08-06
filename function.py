4f56b95d1ff309928ec2e79e708828c6e4b61c3d
Tharindu97 <tharindukavishna@gmail.com>
2021-08-06 17:24:25 +0530

    learn function create

---
+++ function.py
@@ -0,0 +1,17 @@
+# def functionName(arg1):
+#     print(arg1)
+#
+# functionName("Tharindu Kavishna")
+
+# def sum(num1,num2):
+#     return num1 + num2
+#
+# print(sum(4,4))
+
+# def areas(length, width):
+#     perimeter = 2 * (length + width)
+#     area = length * width
+#     print(perimeter)
+#     print(area)
+#
+# areas(5,4)
